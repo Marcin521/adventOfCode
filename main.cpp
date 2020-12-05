8e2b2562aa2ae6ff177c1f808b3e520141176463
Marcin521 <marcinkondo134@gmail.com>
2020-12-05 19:19:42 +0100

    add loading system

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int line;
+    int lNumber=1;
+
+    fstream plik;
+    plik.open("matrix.txt", ios::in);
+
+    if(plik.good()==false)
+    {
+        cout << " file not exist" << endl;
+    }
+
+
+
+
+
+    plik.close();
+
+
     return 0;
 }
